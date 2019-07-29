c3b819accb9516cb62c7e3dde9ca208c9c3d4a19
Aswin G <aswinganesh666@gmail.com>
2019-07-29 17:10:41 +0530

    Update image.py with image processing script

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from src.algorithm.algorithm import algorithm
 
 
 if __name__ == "__main__":
-	t1 = threading.Thread(target=algorithm)
-	t2 = threading.Thread(target=image)
-	
-	t1.start()
-	t2.start()
-	
-	t1.join()
-	t2.join()
+    t1 = threading.Thread(target=algorithm)
+    t2 = threading.Thread(target=image)
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
