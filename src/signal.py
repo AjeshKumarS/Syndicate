1754f572efc1da9fcfd93357daeca32c1b375349
Aswin G <aswingauthor@gmail.com>
2019-08-04 15:57:13 +0530

    Add the emergency response system back in

--- src/signal.py
+++ src/signal.py
@@ -1,20 +1,21 @@
 class Signal:
-	maximum = 90
-	standard = 60
-	minimum = 10
-	lanes = [20, 20, 20, 20]
-	emergency = False
-	target = 1
-	IMAGE_PROCESSING_FREQUENCY = 5
-	
-	@staticmethod
-	def update_timings(threshold):
-		for i in range(4):
-			val = int((Signal.standard * threshold[i])/100)
-			val = max(Signal.minimum, val)
-			Signal.lanes[i] = min(Signal.maximum, val)
-	
-	@staticmethod
-	def timings():
-		for i in range(4):
-			print("Lane {0}: {1}".format(i, Signal.lanes[i]))
+    maximum = 90
+    standard = 60
+    minimum = 10
+    lanes = [20, 20, 20, 20]
+    emergency = False
+    target = 1
+    IMAGE_PROCESSING_FREQUENCY = 5
+    EMERGENCY_CONSTANT = 5	
+    @staticmethod
+    def update_timings(threshold):
+        for i in range(4):
+            val = int((Signal.standard * threshold[i]) / 100)
+            val = max(Signal.minimum, val)
+            Signal.lanes[i] = min(Signal.maximum, val)
+
+    @staticmethod
+    def timings():
+        for i in range(4):
+            print("Lane {0}: {1}".format(i, Signal.lanes[i]))
+
