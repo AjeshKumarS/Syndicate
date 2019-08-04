38853906f54196275d1f442b0a379923dcaa45dc
Aswin G <aswingauthor@gmail.com>
2019-08-04 16:46:01 +0530

    Algorithm now schedules based on priority

--- src/algorithm/algorithm.py
+++ src/algorithm/algorithm.py
@@ -6,13 +6,14 @@ def algorithm():
     # Wait to ensure image processing algorithm runs at least once
     time.sleep(Signal.IMAGE_PROCESSING_FREQUENCY)
     while True:
-        curr_lane = Signal.lanes_priority.index(max(Signal.lanes_priority))
+        curr_lane = Signal.lanes_value.index(max(Signal.lanes_value))
         curr_timing = Signal.lanes_timing[curr_lane]
         print(
             "Lane {0} is given green signal for {1} seconds".format(
                 curr_lane, curr_timing
             )
         )
+        Signal.lanes_priority[curr_lane] = 0
         for i in range(curr_timing):
             if Signal.emergency:
                 while Signal.emergency:
@@ -25,3 +26,4 @@ def algorithm():
                 break
             else:
                 time.sleep(1)
+        Signal.update_priority()
