1b3fe0d2d434ba5c4efce79b81238802006c1edb
Aswin G <aswingauthor@gmail.com>
2019-08-04 15:57:13 +0530

    Add the emergency response system back in

--- src/algorithm/algorithm.py
+++ src/algorithm/algorithm.py
@@ -13,16 +13,15 @@ def algorithm():
                 curr_lane, curr_timing
             )
         )
-        time.sleep(curr_timing)
-        # i = 0
-        # while i < 4:
-        #     Signal.timings()
-        #     print("Current Lane: " + str(i) + "\n")
-        #     for j in range(Signal.lanes[i]):
-        #         if Signal.emergency
-        #             i = Signal.target - 1
-        #             Signal.emergency = False
-        #             break
-        #         else:
-        #             time.sleep(1)
-        #     i = i + 1
+        for i in range(curr_timing):
+            if(Signal.emergency):
+                while Signal.emergency:
+                    print(
+                        "Lane {0} is given green signal for {1} seconds".format(
+                            Signal.target, Signal.EMERGENCY_CONSTANT
+                        )
+                    )
+                    time.sleep(Signal.EMERGENCY_CONSTANT)
+                break
+            else:
+                time.sleep(1)
