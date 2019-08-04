604ee528e4c99ac98ee09cc00672c77d0aeb4a13
Aswin G <aswingauthor@gmail.com>
2019-08-04 14:29:57 +0530

    Modify algorithm to be based on highest density

--- src/algorithm/algorithm.py
+++ src/algorithm/algorithm.py
@@ -6,15 +6,23 @@ def algorithm():
     # Wait to ensure image processing algorithm runs at least once
     time.sleep(Signal.IMAGE_PROCESSING_FREQUENCY)
     while True:
-        i = 0
-        while i < 4:
-            Signal.timings()
-            print("Current Lane: " + str(i) + "\n")
-            for j in range(Signal.lanes[i]):
-                if Signal.emergency:
-                    i = Signal.target - 1
-                    Signal.emergency = False
-                    break
-                else:
-                    time.sleep(1)
-            i = i + 1
+        curr_timing = max(Signal.lanes)
+        curr_lane = Signal.lanes.index(curr_timing)
+        print(
+            "Lane {0} is given green signal for {1} seconds".format(
+                curr_lane, curr_timing
+            )
+        )
+        time.sleep(curr_timing)
+        # i = 0
+        # while i < 4:
+        #     Signal.timings()
+        #     print("Current Lane: " + str(i) + "\n")
+        #     for j in range(Signal.lanes[i]):
+        #         if Signal.emergency
+        #             i = Signal.target - 1
+        #             Signal.emergency = False
+        #             break
+        #         else:
+        #             time.sleep(1)
+        #     i = i + 1
