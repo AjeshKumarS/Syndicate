829c5b27945fc15d66d51ba4655388d28849036c
Aswin M Prabhu <aswinmprabhu@gmail.com>
2019-08-15 13:23:56 +0530

    Add a simulator prototype

--- src/signal.py
+++ src/signal.py
@@ -5,19 +5,22 @@ class Signal:
     lanes_timing = [20, 20, 20, 20]
     lanes_priority = [1, 1, 1, 1]
     lanes_value = [0, 0, 0, 0]
+    lanes_density = [0, 0, 0, 0]
     emergency = False
     lane_with_emergency = 1
     IMAGE_PROCESSING_FREQUENCY = 5
     EMERGENCY_CONSTANT = 5
 
     @staticmethod
-    def update_timings(threshold):
+    def update_timings(densities):
         for i in range(4):
-            val = int((Signal.standard * threshold[i]) / 100)
+            val = int((Signal.standard * densities[i]) / 100)
             val = max(Signal.minimum, val)
             Signal.lanes_timing[i] = min(Signal.maximum, val)
-            Signal.lanes_value[i] = Signal.lanes_timing[i] * Signal.lanes_priority[i]
+            Signal.lanes_value[i] = Signal.lanes_timing[i] * \
+                Signal.lanes_priority[i]
 
+    @staticmethod
     def update_priority():
         for i in range(4):
             Signal.lanes_priority[i] += 1
@@ -26,4 +29,3 @@ class Signal:
     def timings():
         for i in range(4):
             print("Lane {0}: {1}".format(i, Signal.lanes_timing[i]))
-
