abe5c88940745cc6d9fd662f22a7c1c229c162ac
Aswin M Prabhu <aswinmprabhu@gmail.com>
2019-08-17 15:40:25 +0530

    Change the simulator to a non threading model

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import threading
 import sys
 from src.imageProcessing.image import main
-from src.algorithm.algorithm import algorithm
+from src.algorithm.algorithm import run_algorithm
 from src.sound.sound import sound
 from src.simulator.simulator import Simulator
 
 
 if __name__ == "__main__":
-    # if len(sys.argv) > 1 and sys.argv[1] == "-sim":
-    Simulator.simulate()
-    # else:
-    #     t1 = threading.Thread(target=algorithm)
-    #     t2 = threading.Thread(target=main)
-    #     t3 = threading.Thread(target=sound)
+    if len(sys.argv) > 1 and sys.argv[1] == "-sim":
+        Simulator.simulate()
+    else:
+        t1 = threading.Thread(target=run_algorithm)
+        t2 = threading.Thread(target=main)
+        t3 = threading.Thread(target=sound)
 
-    #     t1.start()
-    #     t2.start()
-    #     t3.start()
+        t1.start()
+        t2.start()
+        t3.start()
 
-    #     t1.join()
-    #     t2.join()
-    #     t3.join()
+        t1.join()
+        t2.join()
+        t3.join()
--- src/simulator/simulator.py
+++ src/simulator/simulator.py
@@ -3,40 +3,40 @@ import threading
 import time
 from ..algorithm.algorithm import algorithm
 from ..signal import Signal
+from enum import Enum
 
 
 class Simulator:
     @staticmethod
     def gen_densities():
-        while True:
-            # print("gen_densities is goin to run" +
-            #       str(Signal.curr_lane_to_open))
-            densities = [0, 0, 0, 0]
-            for i in range(4):
-                if i != Signal.curr_lane_to_open:
-                    if Signal.lanes_densities[i] < 80:
-                        densities[i] = Signal.lanes_densities[i] + \
-                            random.randrange(0, 20)
-                    else:
-                        densities[i] = 100
+        densities = [0, 0, 0, 0]
+        for i in range(4):
+            if i != Signal.curr_lane_to_open:
+                if Signal.lanes_densities[i] < 80:
+                    densities[i] = Signal.lanes_densities[i] + \
+                        random.randrange(0, 20)
                 else:
-                    # assuming approx 3 cars pass through each second
-                    # while the light is green
-                    # TODO: optimize based on the timing calculation algo
-                    if densities[i] <= Signal.lanes_duration[Signal.curr_lane_to_open] * (2/5):
-                        densities[i] = 1
-                    else:
-                        densities[i] = Signal.lanes_densities[i] - \
-                            Signal.lanes_duration[Signal.curr_lane_to_open] * \
-                            (2/5)
+                    densities[i] = 100
+            else:
+                # assuming approx 3 cars pass through each second
+                # while the light is green
+                # TODO: optimize based on the timing calculation algo
+                if densities[i] <= Signal.lanes_duration[Signal.curr_lane_to_open] * (2/5):
+                    densities[i] = 1
+                else:
+                    densities[i] = Signal.lanes_densities[i] - \
+                        Signal.lanes_duration[Signal.curr_lane_to_open] * \
+                        (2/5)
 
-            Signal.update_timings(densities)
-            # print("gen_densities ran" + str(Signal.curr_lane_to_open))
-            time.sleep(0.1)
+        Signal.update_timings(densities)
 
     @staticmethod
-    def run_algo():
-        algorithm()
+    def run_algorithm():
+        # Wait to ensure image processing algorithm runs at least once
+        time.sleep(Signal.IMAGE_PROCESSING_FREQUENCY)
+        while True:
+            algorithm()
+            Simulator.gen_densities()
 
     @staticmethod
     def simulate():
@@ -45,11 +45,4 @@ class Simulator:
             densities[i] = random.randrange(0, 100)
         Signal.update_timings(densities)
 
-        algo_thread = threading.Thread(target=Simulator.run_algo)
-        gen_thread = threading.Thread(target=Simulator.gen_densities)
-
-        algo_thread.start()
-        gen_thread.start()
-
-        algo_thread.join()
-        gen_thread.join()
+        Simulator.run_algorithm()
