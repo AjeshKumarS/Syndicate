b5149ad38f02090200a00673f9ae8825a503a1dc
Ajesh Kumar S <ajeshkumar36020@gmail.com>
2019-08-28 11:59:22 +0530

    added Arduino

--- src/bolt/bolt.py
+++ src/bolt/bolt.py
@@ -1,14 +1,13 @@
-from boltiot import Bolt
-
-# api_key = '6febd8b6-a235-4171-b06e-5f380fef9a67'
-# device_id = 'BOLT1119518'
-# my_bolt = Bolt(api_key, device_id)
+# import serial
+# import time
+#
+# ArduinoSerial = serial.Serial('com6', 9600)
+# time.sleep(2)
+#
+# print(ArduinoSerial.readline())
 
 
 def change_signal(next_signal):
+	# ArduinoSerial.write(str.encode(str(next_signal + 1)))
 	pass
-	# for i in range(4):
-	# 	if i != next_signal:
-	# 		my_bolt.digitalWrite(str(i+1), 'LOW')
-	# 	else:
-	# 		my_bolt.digitalWrite(str(i+1), 'HIGH')
+
--- src/simulator/simulator.py
+++ src/simulator/simulator.py
@@ -7,42 +7,42 @@ from enum import Enum
 
 
 class Simulator:
-    @staticmethod
-    def gen_densities():
-        densities = [0, 0, 0, 0]
-        for i in range(4):
-            if i != Signal.curr_lane_to_open:
-                if Signal.lanes_densities[i] < 80:
-                    densities[i] = Signal.lanes_densities[i] + \
-                        random.randrange(0, 20)
-                else:
-                    densities[i] = 100
-            else:
-                # assuming approx 3 cars pass through each second
-                # while the light is green
-                # TODO: optimize based on the timing calculation algo
-                if densities[i] <= Signal.lanes_duration[Signal.curr_lane_to_open] * (2/5):
-                    densities[i] = 1
-                else:
-                    densities[i] = Signal.lanes_densities[i] - \
-                        Signal.lanes_duration[Signal.curr_lane_to_open] * \
-                        (2/5)
-
-        Signal.update_timings(densities)
-
-    @staticmethod
-    def run_algorithm():
-        # Wait to ensure image processing algorithm runs at least once
-        time.sleep(Signal.IMAGE_PROCESSING_FREQUENCY)
-        while True:
-            algorithm()
-            Simulator.gen_densities()
-
-    @staticmethod
-    def simulate():
-        densities = [0, 0, 0, 0]
-        for i in range(4):
-            densities[i] = random.randrange(0, 100)
-        Signal.update_timings(densities)
-
-        Simulator.run_algorithm()
+	@staticmethod
+	def gen_densities():
+		densities = [0, 0, 0, 0]
+		for i in range(4):
+			if i != Signal.curr_lane_to_open:
+				if Signal.lanes_densities[i] < 80:
+					densities[i] = Signal.lanes_densities[i] + \
+						random.randrange(0, 20)
+				else:
+					densities[i] = 100
+			else:
+				# assuming approx 3 cars pass through each second
+				# while the light is green
+				# TODO: optimize based on the timing calculation algo
+				if densities[i] <= Signal.lanes_duration[Signal.curr_lane_to_open] * (2 / 10):
+					densities[i] = 1
+				else:
+					densities[i] = Signal.lanes_densities[i] - \
+							Signal.lanes_duration[Signal.curr_lane_to_open] * \
+							(2 / 10)
+		
+		Signal.update_timings(densities)
+	
+	@staticmethod
+	def run_algorithm():
+		# Wait to ensure image processing algorithm runs at least once
+		time.sleep(Signal.IMAGE_PROCESSING_FREQUENCY)
+		while True:
+			algorithm()
+			Simulator.gen_densities()
+	
+	@staticmethod
+	def simulate():
+		densities = [0, 0, 0, 0]
+		for i in range(4):
+			densities[i] = random.randrange(5, 100)
+		Signal.update_timings(densities)
+		
+		Simulator.run_algorithm()
