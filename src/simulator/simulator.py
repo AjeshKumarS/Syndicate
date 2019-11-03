6acbaa730a1cebae8f5c4e7f8279d650c326df3b
Aswin M Prabhu <aswinmprabhu@gmail.com>
2019-11-03 22:17:47 +0530

    Add logging for debugging perf

--- src/simulator/simulator.py
+++ src/simulator/simulator.py
@@ -7,6 +7,8 @@ from enum import Enum
 
 
 density_reduction_rate = 2/20
+
+
 class Simulator:
     @staticmethod
     def gen_densities():
@@ -54,16 +56,20 @@ class Simulator:
         avg_algo = 0.00
         sum_time_fixed = 0
         avg_fixed = 0.00
-        for i in range(1):
+        no_of_cases = 1
+        fixed_duration = 120
+        for i in range(no_of_cases):
             densities = [0, 0, 0, 0]
             for j in range(4):
                 densities[j] = random.randrange(5, 100)
+            print("densities : ", densities)
             Signal.update_timings(densities)
             algo_densities = densities[:]
             lanes_done = 0
             while lanes_done != 4:
                 curr_lane = Signal.curr_lane_to_open
                 curr_timing = Signal.lanes_duration[curr_lane]
+                print("clane: ", curr_lane, "ctime: ", curr_timing)
                 sum_time_algo += curr_timing
                 if algo_densities[curr_lane] > 0:
                     if algo_densities[curr_lane] - (density_reduction_rate) * curr_timing > 0:
@@ -71,24 +77,28 @@ class Simulator:
                             (density_reduction_rate) * curr_timing
                     else:
                         algo_densities[curr_lane] = 0
-                    if algo_densities[curr_lane] == 0:
+                    if algo_densities[curr_lane] <= 0:
                         lanes_done += 1
                 Signal.update_priority(curr_timing, curr_lane)
+                print("densities : ", algo_densities)
 
             # Fixed : 30 sec for 1 lane
             lanes_done = 0
             while lanes_done != 4:
                 for j in range(len(densities)):
                     if densities[j] > 0:
-                        sum_time_fixed += 120
-                        if densities[j] - (density_reduction_rate) * 120 > 0:
-                            densities[j] = densities[j] - (density_reduction_rate) * 120
+                        sum_time_fixed += fixed_duration
+                        if densities[j] - (density_reduction_rate) * fixed_duration > 0:
+                            densities[j] = densities[j] - \
+                                (density_reduction_rate) * fixed_duration
                         else:
                             densities[j] = 0
                         if densities[j] <= 0:
                             lanes_done += 1
-        avg_fixed = float(sum_time_fixed/1)
-        avg_algo = float(sum_time_algo/1)
+                    print("dens,ities : ", densities)
+        print("sft: ", sum_time_fixed, "sat: ", sum_time_algo)
+        avg_fixed = float(sum_time_fixed/no_of_cases)
+        avg_algo = float(sum_time_algo/no_of_cases)
         print("Avg time taken to empty all lanes in")
         print("Fixed : ", avg_fixed)
         print("Algo : ", avg_algo)
