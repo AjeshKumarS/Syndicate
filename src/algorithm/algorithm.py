c8ae4f5f5a187a263259a32d5aa55b70d898552e
Aswin M Prabhu <aswinmprabhu@gmail.com>
2019-11-03 08:31:14 +0530

    Finish perf reporting

--- src/algorithm/algorithm.py
+++ src/algorithm/algorithm.py
@@ -4,35 +4,37 @@ from ..bolt.bolt import change_signal
 
 
 def algorithm():
-	curr_lane = Signal.curr_lane_to_open
-	curr_timing = Signal.lanes_duration[curr_lane]
-	change_signal(curr_lane)
-	print(Signal.lanes_densities, Signal.lanes_waiting_time)
-	print(
-		"Lane {0} is given green signal for {1} seconds".format(
-			curr_lane + 1, curr_timing
-		)
-	)
-	
-	for i in range(curr_timing):
-		if Signal.emergency:
-			while Signal.emergency:
-				change_signal(curr_lane)
-				print(
-					"Lane {0} is given green signal for {1} seconds".format(
-						Signal.lane_with_emergency + 1, Signal.EMERGENCY_CONSTANT
-					)
-				)
-				time.sleep(Signal.EMERGENCY_CONSTANT)
-			break
-		else:
-			# time passed in reduced to 0.3 sec just for testing
-			time.sleep(0.3)
-	Signal.update_priority(curr_timing, curr_lane)
+    curr_lane = Signal.curr_lane_to_open
+    curr_timing = Signal.lanes_duration[curr_lane]
+    change_signal(curr_lane)
+    print(Signal.lanes_densities, Signal.lanes_waiting_time)
+    print(
+        "Lane {0} is given green signal for {1} seconds".format(
+            curr_lane + 1, curr_timing
+        )
+    )
+    print("Prio : ", Signal.lanes_priority)
+    print("WT : ", Signal.lanes_waiting_time)
+
+    for i in range(curr_timing):
+        if Signal.emergency:
+            while Signal.emergency:
+                change_signal(curr_lane)
+                print(
+                    "Lane {0} is given green signal for {1} seconds".format(
+                        Signal.lane_with_emergency + 1, Signal.EMERGENCY_CONSTANT
+                    )
+                )
+                time.sleep(Signal.EMERGENCY_CONSTANT)
+            break
+        else:
+            # time passed in reduced to 0.3 sec just for testing
+            time.sleep(0.3)
+    Signal.update_priority(curr_timing, curr_lane)
 
 
 def run_algorithm():
-	# Wait to ensure image processing algorithm runs at least once
-	time.sleep(Signal.IMAGE_PROCESSING_FREQUENCY)
-	while True:
-		algorithm()
+    # Wait to ensure image processing algorithm runs at least once
+    time.sleep(Signal.IMAGE_PROCESSING_FREQUENCY)
+    while True:
+        algorithm()
--- src/signal.py
+++ src/signal.py
@@ -2,49 +2,52 @@ import math
 
 
 class Signal:
-	lanes_densities = [0, 0, 0, 0]
-	lanes_waiting_time = [0, 0, 0, 0]
-	lanes_priority = [0, 0, 0, 0]
-	lanes_duration = [0, 0, 0, 0]
-	prev_lane_that_was_open = None
-	curr_lane_to_open = None
-	maximum_waiting_time = 70
-	minimum_green_duraiton = 20
-	emergency = False
-	lane_with_emergency = 1
-	IMAGE_PROCESSING_FREQUENCY = 5
-	EMERGENCY_CONSTANT = 5
-	
-	@staticmethod
-	def calculate_priority(waiting_time, density):
-		waiting_time_fraction = waiting_time / Signal.maximum_waiting_time
-		return math.log((math.pow(waiting_time_fraction, 4) + density) / 10, 10)
-	
-	@staticmethod
-	def update_timings(densities):
-		Signal.lanes_densities = densities
-		for i in range(4):
-			priority = Signal.calculate_priority(
-				Signal.lanes_waiting_time[i], Signal.lanes_densities[i])
-			Signal.lanes_priority[i] = priority
-			Signal.lanes_duration[i] = int(
-				0.6 * Signal.lanes_densities[i] + 20)
-		Signal.prev_lane_that_was_open = Signal.curr_lane_to_open
-		Signal.curr_lane_to_open = Signal.lanes_priority.index(
-			max(Signal.lanes_priority))
-	
-	@staticmethod
-	def update_priority(waiting_duration, lane_not_waiting):
-		for i in range(4):
-			if i != lane_not_waiting:
-				Signal.lanes_waiting_time[i] += waiting_duration
-			if i == lane_not_waiting:
-				Signal.lanes_waiting_time[i] = 0
-			priority = Signal.calculate_priority(
-				Signal.lanes_waiting_time[i], Signal.lanes_densities[i])
-			Signal.lanes_priority[i] = priority
-	
-	@staticmethod
-	def timings():
-		for i in range(4):
-			print("Lane {0}: {1}".format(i, Signal.lanes_duration[i]))
+    lanes_densities = [0, 0, 0, 0]
+    lanes_waiting_time = [0, 0, 0, 0]
+    lanes_priority = [0, 0, 0, 0]
+    lanes_duration = [0, 0, 0, 0]
+    prev_lane_that_was_open = None
+    curr_lane_to_open = None
+    maximum_waiting_time = 70
+    minimum_green_duration = 20
+    emergency = False
+    lane_with_emergency = 1
+    IMAGE_PROCESSING_FREQUENCY = 5
+    EMERGENCY_CONSTANT = 5
+
+    @staticmethod
+    def calculate_priority(waiting_time, density):
+        waiting_time_fraction = waiting_time / Signal.maximum_waiting_time
+        return (math.pow(waiting_time_fraction, 4) + density) / 10
+
+    @staticmethod
+    def update_timings(densities):
+        Signal.lanes_densities = densities
+        for i in range(4):
+            priority = Signal.calculate_priority(
+                Signal.lanes_waiting_time[i], Signal.lanes_densities[i])
+            Signal.lanes_priority[i] = priority
+            Signal.lanes_duration[i] = int(max(
+                0.6 * Signal.lanes_densities[i], Signal.minimum_green_duration))
+        Signal.prev_lane_that_was_open = Signal.curr_lane_to_open
+        Signal.curr_lane_to_open = Signal.lanes_priority.index(
+            max(Signal.lanes_priority))
+
+    @staticmethod
+    def update_priority(waiting_duration, lane_not_waiting):
+        for i in range(4):
+            if i != lane_not_waiting:
+                Signal.lanes_waiting_time[i] += waiting_duration
+            if i == lane_not_waiting:
+                Signal.lanes_waiting_time[i] = 0
+            priority = Signal.calculate_priority(
+                Signal.lanes_waiting_time[i], Signal.lanes_densities[i])
+            Signal.lanes_priority[i] = priority
+        Signal.prev_lane_that_was_open = Signal.curr_lane_to_open
+        Signal.curr_lane_to_open = Signal.lanes_priority.index(
+            max(Signal.lanes_priority))
+
+    @staticmethod
+    def timings():
+        for i in range(4):
+            print("Lane {0}: {1}".format(i, Signal.lanes_duration[i]))
