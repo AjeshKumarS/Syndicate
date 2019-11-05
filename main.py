04047e7cf945b5337db9142094d7ffda36392688
Aswin G <aswinganesh666@gmail.com>
2019-11-06 00:26:48 +0530

    Initial work for visualisation done

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from src.algorithm.algorithm import run_algorithm
 from src.sound.sound import sound
 from src.simulator.simulator import Simulator
 
-
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "-sim":
         Simulator.simulate()
     elif len(sys.argv) > 1 and sys.argv[1] == "-perf":
         Simulator.simulate_with_perf()
+    elif len(sys.argv) > 1 and sys.argv[1] == "-visuals":
+        # The flask server MUST be running to use this option
+        Simulator.simulate_with_visuals()
     else:
         t1 = threading.Thread(target=run_algorithm)
         t2 = threading.Thread(target=main)
--- src/simulator/simulator.py
+++ src/simulator/simulator.py
@@ -4,9 +4,10 @@ import time
 from ..algorithm.algorithm import algorithm
 from ..signal import Signal
 from enum import Enum
+import urllib.request
 
 
-density_reduction_rate = 2/5
+density_reduction_rate = 2 / 5
 
 
 class Simulator:
@@ -16,17 +17,18 @@ class Simulator:
         for i in range(4):
             if i != Signal.curr_lane_to_open:
                 if Signal.lanes_densities[i] < 80:
-                    densities[i] = Signal.lanes_densities[i] + \
-                        random.randrange(0, 20)
+                    densities[i] = Signal.lanes_densities[i] + random.randrange(0, 20)
                 else:
                     densities[i] = 100
             else:
-                if densities[i] <= Signal.lanes_duration[Signal.curr_lane_to_open] * (density_reduction_rate):
+                if densities[i] <= Signal.lanes_duration[Signal.curr_lane_to_open] * (
+                    density_reduction_rate
+                ):
                     densities[i] = 1
                 else:
-                    densities[i] = Signal.lanes_densities[i] - \
-                        Signal.lanes_duration[Signal.curr_lane_to_open] * \
-                        (density_reduction_rate)
+                    densities[i] = Signal.lanes_densities[i] - Signal.lanes_duration[
+                        Signal.curr_lane_to_open
+                    ] * (density_reduction_rate)
 
         curr_lane = Signal.curr_lane_to_open
         curr_timing = Signal.lanes_duration[curr_lane]
@@ -50,6 +52,19 @@ class Simulator:
         Simulator.run_algorithm()
 
     @staticmethod
+    def simulate_with_visuals():
+        BASE_URL = "http://127.0.0.1:5000/setDensities?"
+        densities = [0, 0, 0, 0]
+        URL = BASE_URL
+        for i in range(4):
+            densities[i] = random.randrange(5, 100)
+            URL = URL + "l" + str(i + 1) + "=" + str(densities[i]) + "&"
+        res = urllib.request.urlopen(URL)
+        print(res)
+        Signal.update_timings(densities)
+        Simulator.run_algorithm()
+
+    @staticmethod
     def simulate_with_perf():
         sum_time_algo = 0
         avg_algo = 0.00
@@ -69,9 +84,15 @@ class Simulator:
                 curr_timing = Signal.lanes_duration[curr_lane]
                 sum_time_algo += curr_timing
                 if algo_densities[curr_lane] > 0:
-                    if algo_densities[curr_lane] - (density_reduction_rate) * curr_timing > 0:
-                        algo_densities[curr_lane] = algo_densities[curr_lane] - \
-                            (density_reduction_rate) * curr_timing
+                    if (
+                        algo_densities[curr_lane]
+                        - (density_reduction_rate) * curr_timing
+                        > 0
+                    ):
+                        algo_densities[curr_lane] = (
+                            algo_densities[curr_lane]
+                            - (density_reduction_rate) * curr_timing
+                        )
                     else:
                         algo_densities[curr_lane] = 0
                     if algo_densities[curr_lane] <= 0:
@@ -84,14 +105,15 @@ class Simulator:
                     sum_time_fixed += fixed_duration
                     if densities[j] > 0:
                         if densities[j] - (density_reduction_rate) * fixed_duration > 0:
-                            densities[j] = densities[j] - \
-                                (density_reduction_rate) * fixed_duration
+                            densities[j] = (
+                                densities[j] - (density_reduction_rate) * fixed_duration
+                            )
                         else:
                             densities[j] = 0
                         if densities[j] <= 0:
                             lanes_done += 1
-        avg_fixed = float(sum_time_fixed/no_of_cases)
-        avg_algo = float(sum_time_algo/no_of_cases)
+        avg_fixed = float(sum_time_fixed / no_of_cases)
+        avg_algo = float(sum_time_algo / no_of_cases)
         print("Avg time taken to empty all lanes in")
         print("Fixed : ", avg_fixed)
         print("Algo : ", avg_algo)
